d45a8fa3253bb534eb81f707b6ab1eb5f0248be5
eezra <50751393+eezra@users.noreply.github.com>
2019-05-19 11:45:03 -0400

    Update h.php

--- h.php
+++ h.php
@@ -1,1 +1,15 @@
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    echo("hello world");
+     ?>
+
+  </body>
+</html>
